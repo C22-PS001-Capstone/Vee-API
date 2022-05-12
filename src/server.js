69d4b48b7fc36656a0e392d39f393d14d1a54f38
yanFajR <ryan.fajri045@mail.ugm.ac.id>
2022-05-12 15:10:21 +0700

    initial project commit

---
+++ src/server.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
+const Hapi = require('@hapi/hapi');
+
+require('dotenv').config();
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
