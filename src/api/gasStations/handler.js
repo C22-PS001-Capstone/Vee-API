14bd2fddf9eab4c6b7b8d018d857b3baa7a6474a
yanFajR <ryan.fajri045@mail.ugm.ac.id>
2022-05-23 21:31:39 +0700

    edit gasstation response

--- src/api/gasStations/handler.js
+++ src/api/gasStations/handler.js
@@ -1,9 +1,31 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable no-mixed-operators */
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 const fetch = require('node-fetch');
 const ClientError = require('../../exceptions/ClientError');
+
+function distance(lat1, lon1, lat2, lon2) {
+  if ((lat1 === lat2) && (lon1 === lon2)) {
+    return 0;
+  }
+
+  const radlat1 = Math.PI * lat1 / 180;
+  const radlat2 = Math.PI * lat2 / 180;
+  const theta = lon1 - lon2;
+  const radtheta = Math.PI * theta / 180;
+  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  if (dist > 1) {
+    dist = 1;
+  }
+  dist = Math.acos(dist);
+  dist = dist * 180 / Math.PI;
+  dist = dist * 60 * 1.1515;
+  dist *= 1.609344;
+  return dist;
+}
 
 class GasStationsHandler {
   constructor(validator) {
@@ -19,12 +41,23 @@ class GasStationsHandler {
       const fetchResponse = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/fuel.json?type=poi&proximity=${lon},${lat}&access_token=${process.env.MAPBOX_API_KEY}`);
       const jsonResponse = await fetchResponse.json();
 
+      const gasstations = [];
+
+      jsonResponse.features.forEach((gs) => {
+        gasstations.push({
+          place_name: gs.place_name,
+          address: gs.properties.address,
+          vendor: gs.text,
+          distance: distance(lat, lon, gs.geometry.coordinates[1], gs.geometry.coordinates[0]),
+          latitude: gs.geometry.coordinates[1],
+          longitude: gs.geometry.coordinates[0],
+        });
+      });
+
       const response = h.response({
         status: 'success',
         message: 'Gas Stations terdekat berhasil didapatkan',
-        data: {
-          nearestGasStations: jsonResponse.features,
-        },
+        data: gasstations,
       });
       response.code(200);
       response.message('Gas Stations terdekat berhasil didapatkan');
